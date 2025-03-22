2b5aaf9f41edaf2f44cd3cb93829b79cf451a9f5
fantasticlion <den20020805@gmail.com>
2025-03-22 03:47:51 +0300

    init

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -11,8 +11,8 @@ def test_get_mask_card_number() -> None:
 
     # Тестирование исключений
     with pytest.raises(ValueError, match="Card number is too short"):
-        get_mask_card_number("215412214212125")  # Слишком короткий номер
 
+        get_mask_card_number("215412214212125")  # Слишком короткий номер
     with pytest.raises(ValueError, match="Card number is empty"):
         get_mask_card_number("")  # Пустая строка
 
@@ -20,13 +20,22 @@ def test_get_mask_card_number() -> None:
 def test_get_mask_account() -> None:
     """Функция тестирования масок счетов"""
 
-    # Тесты на корректное поведение функции
-    assert get_mask_account("12345678901234567890") == "****567890"
-    assert get_mask_account("98765432109876543210") == "****432110"
+    # Корректное поведение с различной длиной строк
+    assert get_mask_account("1234567890") == "****7890", "Ожидаемая маска '****7890'"
+    assert get_mask_account("12345678901234567890") == "****567890", "Ожидаемая маска '****567890'"
 
-    # Тестирование исключений
-    with pytest.raises(ValueError, match="Account number is too short"):
-        get_mask_account("12345")  # Слишком короткий номер
+    # Пограничный случай с минимальным допустимым количеством символов (4)
+    assert get_mask_account("1234") == "****1234", "Ожидаемая маска '****1234'"
 
-    with pytest.raises(ValueError, match="Account number is empty"):
+    # Тест на выброс исключений
+    with pytest.raises(ValueError, match="Account number must contain only digits."):
+        get_mask_account("12a45")  # Содержит недопустимые символы
+
+    with pytest.raises(ValueError, match="Account number must contain only digits."):
+        get_mask_account(" ")  # Пустое значение
+
+    with pytest.raises(ValueError, match="Номер счёта должен содержать минимум 4 цифры."):
+        get_mask_account("123")  # Слишком короткий номер
+
+    with pytest.raises(ValueError, match="Номер счёта должен содержать минимум 4 цифры."):
         get_mask_account("")  # Пустая строка
